46005628826d5a0eee0fb648ef0154c79d01c630
Ghassan Shahzad <ghass.shahzad@gmail.com>
2023-06-14 01:26:51 +0500

    restyled, added a bunch more subjects, and tested

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import re
 
 app = Flask(__name__)
 
+
 def extract_answers(ms):
     """Takes the marking scheme as a parameter, goes through it, and stores all its contents in a string."""
     answers = ''
@@ -19,11 +20,13 @@ def extract_answers(ms):
 
     return answers
 
+
 def ans_filter(raw):
     """Takes the string generated in extract_answers() and filters it to extract all MCQ answers in a list (by extracting all individual letters)"""
     raw_list = re.findall("[a-zA-Z]+", raw)
-    lst = [x for x in raw_list if len(x) == 1] 
+    lst = [x for x in raw_list if len(x) == 1]
     return lst
+
 
 def download_pdfs(subj, year, month, variant, ce):
     """Takes the inputs from the forms on the homepage and uses them to download the corresponding question paper and marking scheme."""
@@ -39,18 +42,23 @@ def download_pdfs(subj, year, month, variant, ce):
         if char == ')':
             flag = False
         if flag == True:
-            subj_code+=char
-     
-    url_ms = f"https://papers.gceguide.com/Cambridge IGCSE/{subj}/{year}/{subj_code}_{month}{str(year)[2:4]}_ms_{ce}{variant}.pdf"
-    url_qp = f"https://papers.gceguide.com/Cambridge IGCSE/{subj}/{year}/{subj_code}_{month}{str(year)[2:4]}_qp_{ce}{variant}.pdf"
+            subj_code += char
+    if ce == None:
+        ce = 1
+
+    url_ms = f"https://papers.gceguide.com/{subj}/{year}/{subj_code}_{month}{str(year)[2:4]}_ms_{ce}{variant}.pdf"
+    url_qp = f"https://papers.gceguide.com/{subj}/{year}/{subj_code}_{month}{str(year)[2:4]}_qp_{ce}{variant}.pdf"
+    print(url_ms, url_qp)
     r = requests.get(url_qp, allow_redirects=True)
     open(os.path.join(root_dir, 'static', 'pdf', 'qp.pdf'), 'wb').write(r.content)
     r = requests.get(url_ms, allow_redirects=True)
     open(os.path.join(root_dir, 'static', 'pdf', 'ms.pdf'), 'wb').write(r.content)
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/pdf", methods=['POST', 'GET'])
 def pdf_display():
@@ -67,25 +75,27 @@ def pdf_display():
         download_pdfs(subj, year, month, variant, ce)
         raw_mess = extract_answers('static/pdf/ms.pdf')
         answers = ans_filter(raw_mess)
-        
+
         question_number = 0
         score = 0  # Initialize the score to 0
         return render_template('pdf.html', question_number=question_number, answers=answers, score=score)
+
 
 @app.route('/static/pdf/<path:filename>')
 def serve_pdf(filename):
     root_dir = os.getcwd()
     return send_from_directory(os.path.join(root_dir, 'static', 'pdf'), filename)
 
+
 @app.route("/pdf_answers", methods=['POST'])
 def pdf_answers():
     question_number = int(request.form.get('question_number'))
     user_answer = request.form.get('user_answer')
-    
+
     # Load the correct answers from the marking scheme
     raw_mess = extract_answers('static/pdf/ms.pdf')
     correct_answers = ans_filter(raw_mess)
-    print(user_answer) 
+    print(user_answer)
     # Compare user's answer with the correct answer
     if user_answer.lower() == correct_answers[question_number].lower():
         # Correct answer
@@ -95,14 +105,15 @@ def pdf_answers():
         # Incorrect answer
         score = int(request.form.get('score'))
         feedback = "Incorrect."
-    
+
     next_question_number = question_number + 1
-    
+
     if next_question_number >= len(correct_answers):
         # The user has answered all the questions
         return render_template('pdf_score.html', score=score, total_questions=len(correct_answers))
     print(score)
     return render_template('pdf.html', question_number=next_question_number, answers=correct_answers, score=score, feedback=feedback)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
