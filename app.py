e775029ced49cf5c7669328504f697a7474afc94
Ghassan Shahzad <ghass.shahzad@gmail.com>
2023-07-02 17:54:57 +0500

    feat: :sparkles: added papers/scores tracking; stored in cookies
    Added statistics page that tracks the papers solved and the scores obtained, all stored in cookies.

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask, render_template, request, send_from_directory
-import requests
 import os
-from PyPDF2 import PdfReader
 import re
-
+import json
+from flask import Flask, render_template, request, send_from_directory, make_response
+import requests
+from PyPDF2 import PdfReader
 
 # Creates flask app
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 answers = {}
+papers = {}
 
 
 def extract_answers(ms):
     """Takes the marking scheme as a parameter, goes through it, and stores all its contents in a 'dump'."""
-    answers = ''
+    mcqanswers = ''
     with open(ms, 'rb') as file:
         reader = PdfReader(file)
         for pageno in range(1, len(reader.pages)):
-            answers += (reader.pages[pageno]).extract_text()
-    return answers
+            mcqanswers += (reader.pages[pageno]).extract_text()
+    return mcqanswers
 
 
 def ans_filter(raw):
-    """Takes the dump generated in extract_answers() and filters it to extract all MCQ answers in a list (by extracting all individual letters)"""
+    """Takes the dump generated in extract_answers() and filters it to extract all MCQ answers in a list (by extracting all individual letters through regex)"""
     raw_list = re.findall(r'\b[A-D]\b', raw)
     lst = [x for x in raw_list if len(x) == 1]
     return lst
@@ -55,6 +57,26 @@ def download_pdfs(subj, year, month, variant, ce):
     return url_qp
 
 
+def get_paper_name(subj, year, month, variant, ce):
+    """Gets paper name, for statistics and pdf.html title"""
+    subj_code = ''
+    flag = False
+
+    if ce is None:
+        ce = 1
+
+    for char in subj:
+        if char == '(':
+            flag = True
+            continue
+        if char == ')':
+            flag = False
+        if flag == True:
+            subj_code += char
+
+    return f"{subj_code}_{month}{str(year)[2:4]}_qp_{ce}{variant}"
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -62,7 +84,6 @@ def index():
 
 @app.route("/pdf", methods=['GET', 'POST'])
 def pdf_display():
-    global pdf_link
     if request.method == 'GET':
         return "The URL / is accessed directly. Try going to '/' to submit the form."
 
@@ -76,10 +97,18 @@ def pdf_display():
         pdf_link = download_pdfs(subj, year, month, variant, ce)
         raw_mess = extract_answers('static/pdf/ms.pdf')
         answers = ans_filter(raw_mess)
+        paper_name = get_paper_name(subj, year, month, variant, ce)
 
         question_number = 0
         score = 0  # Initialize the score to 0
-        return render_template('pdf.html', question_number=question_number, answers=answers, score=score, pdf_link=pdf_link)
+
+        papers_json = request.cookies.get('papers_scores')
+        papers = json.loads(papers_json) if papers_json else {}
+
+        resp = make_response(render_template('pdf.html', question_number=question_number, answers=answers, score=score, pdf_link=pdf_link, paper_name=paper_name))
+        resp.set_cookie('papers_scores', json.dumps(papers))
+
+        return resp
 
 
 @app.route('/static/pdf/<path:filename>')
@@ -93,9 +122,13 @@ def pdf_answers():
     pdf_link = request.form.get('pdf_link')
     question_number = int(request.form.get('question_number'))
     user_answer = request.form.get('user_answer')
+    paper_name = request.form.get('paper_name')
 
     raw_mess = extract_answers('static/pdf/ms.pdf')
     correct_answers = ans_filter(raw_mess)
+
+    papers_json = request.cookies.get('papers_scores')
+    papers = json.loads(papers_json) if papers_json else {}
 
     answers[question_number] = user_answer
     # Compare user's answer with the correct answer
@@ -111,10 +144,26 @@ def pdf_answers():
     next_question_number = question_number + 1
 
     if next_question_number >= len(correct_answers):
-        # The user has answered all the questions
-        return render_template('pdf_score.html', score=score, total_questions=len(correct_answers), answers=answers, correct_answers=correct_answers)
-    return render_template('pdf.html', question_number=next_question_number, answers=correct_answers, score=score, feedback=feedback, pdf_link=pdf_link)
+        papers[paper_name] = round((score/len(correct_answers))*100, 1)
+
+        resp = make_response(render_template('pdf_score.html', score=score, total_questions=len(correct_answers), answers=answers, correct_answers=correct_answers))
+        resp.set_cookie('papers_scores', json.dumps(papers))
+
+        return resp
+    resp = make_response(render_template('pdf.html', question_number=next_question_number, answers=correct_answers, score=score, feedback=feedback, pdf_link=pdf_link, paper_name=paper_name))
+    resp.set_cookie('papers_scores', json.dumps(papers))
+
+    return resp
+
+
+@app.route('/statistics')
+def statistics():
+    papers_json = request.cookies.get('papers_scores')
+    papers = json.loads(papers_json) if papers_json else {}
+
+    return render_template('statistics.html', papers=papers)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
